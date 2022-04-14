362d1ca4c55c0a66e571f112e77849f10899e04c
Fernanda Jorge <nanda.jorge@outlook.com>
2022-04-14 16:06:42 -0300

    Modulo3 ate aula 14

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,36 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import com.algaworks.algafood.domain.modal.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+
+public class CozinhaRepositoryImpl implements CozinhaRepository{
+
+	@Override
+	public List<Cozinha> listaTodas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cozinha porId(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cozinha salvar(Cozinha cozinha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remover(Cozinha cozinha) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
