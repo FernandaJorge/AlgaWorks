f46396572f95cb6c8013a169ef2a366b22ef316a
FernandaJorge <nanda.jorge@outlook.com>
2022-06-10 14:49:06 -0300

    ajustes aula 14

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,21 +1,20 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
-import org.springframework.data.jpa.repository.query.JpaQueryLookupStrategy;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.modal.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
-
-import lombok.var;
 
 
 @Repository
@@ -25,34 +24,28 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	private EntityManager manager;
 	
 	@Override
-	public List<Restaurante> find(String nome, 
-			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 		
-		var jpql = new StringBuilder();
-		jpql.append("from Restaurante where 0 = 0 ");
 		
-		var parametros = new HashMap<String, Object>();
+		//o CriteriaBuilder é uma biblioteca que tem vários atributos que podemos usar em uma consulta avançada
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
-		if (StringUtils.hasLength(nome)) {
-			jpql.append("and nome like :nome ");
-			parametros.put("nome", "%" + nome + "%");
-		}
+		// Aqui eu instancio meu criteria passando a classe que eu vou realizar a consulta
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		Root<Restaurante> root = criteria.from(Restaurante.class);//from Restaurante retorna um Root - representa a raiz então o Restaurante
 		
-		if (taxaFreteInicial != null) {
-			jpql.append("and taxaFrete >= :taxaInicial ");
-			parametros.put("taxaInicial", taxaFreteInicial);
-		}
+		//dentro do criteria temos o where que recebe predicados como x = y and x >= 0. Para criar esses predicados,
+		//temos que criar uma variavel do tipo o Predicate, usando do bulder pegamos a instrução like e passamos 
+		//a coluna que vamos filtar e o que vamos usar como parametro pra pesquisa
+		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%" );
+		Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+		Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
 		
-		if (taxaFreteFinal != null) {
-			jpql.append("and taxaFrete <= :taxaFinal ");
-			parametros.put("taxaFinal", taxaFreteFinal);
-		}
 		
-		TypedQuery<Restaurante> query = manager
-				.createQuery(jpql.toString(), Restaurante.class);
+		//depois passo para o where os predicados que vou usar como filtro
+		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
 		
-		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-
+		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
 	}
 
