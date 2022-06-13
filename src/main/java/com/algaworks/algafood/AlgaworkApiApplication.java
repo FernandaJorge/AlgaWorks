345d452f53bd1e4810215e4615eeb3aec50ed4be
Fernanda Jorge <nanda.jorge@outlook.com>
2022-06-13 14:54:34 -0300

    customizar o repositório base

--- src/main/java/com/algaworks/algafood/AlgaworkApiApplication.java
+++ src/main/java/com/algaworks/algafood/AlgaworkApiApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class AlgaworkApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlgaworkApiApplication.class, args);
-		
 	}
 
 }
