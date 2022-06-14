5fe9f7172a0fbec74a3295778aadeaee91a5402b
FernandaJorge <nanda.jorge@outlook.com>
2022-06-14 17:25:16 -0300

    Mapeando classes incorporáveis com @Embedded e @Embeddable e o impacto da incorporação de classe na REST API

--- src/main/java/com/algaworks/algafood/domain/modal/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/modal/Restaurante.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -40,6 +41,9 @@ public class Restaurante {
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
 	
+	@JsonIgnore
+	@Embedded
+	private Endereco endereco;
 	
 	@JsonIgnore
 	@ManyToMany //Incdica que muitos restaurantes possuem muitas formas de pagamento
